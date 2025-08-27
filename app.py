72941094ce1dff254e3ddc8c875c32aff98eda74
Anis Guechtouli <a.guechtouli@univ-alger.dz>
2025-08-28 00:48:12 +0100

    starting the work on the interface

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import pandas as pd
+import joblib
+
+@st.cache_resource
+def load_model():
+    return joblib.load("model/logreg_model.pkl")
+
+model = load_model()
+
+st.set_page_config(page_title="Telecom Churn Prediction",
+                   layout='wide',
+                   initial_sidebar_state='collapsed')
+
+col1, col2 = st.columns([1,1], gap='small', border=True)
+
+col1.title('Insert Client Info')
+
+co1, co2, co3 = col1.columns([1,1,1], gap='small')
+
+gender = co1.selectbox("Gender",
+                    ('Male','Female'))
+
+SeniorCitizen = co2.radio('Senior Citizen',
+                        ('Yes', 'No'),
+                        horizontal=True)
+
+InternetService = co1.selectbox("Internet Service",
+                            ("No internet service", "DSL", "Fibre optic"),
+                            0)
+if InternetService!='No internet service':    
+    MultipleLines = co2.selectbox("Multiple Lines",
+                            ("No phone service", "No", "Yes"),
+                            0)
+
+c1, c2, c3 = st.columns([1.5, 1, 1.5])
+
+## Extract logical relations between variables
+## if no internet -> all internet vars "NO" disabled
+## If no total charges and tenure and monthly charges -> calculate it
+## if tenure = 0  and total charges > 0 -> error
+## If no phone -> multiple lines = no phone
+## senior citizen = 0 and dependent -> error?
+## 
--- src/preprocessing.py
+++ src/preprocessing.py
@@ -19,26 +19,63 @@ TO_DROP = ['No','Mailed check']
 
 TO_SCALE = ['tenure','MonthlyCharges','TotalCharges']
 
-def mapping(df : pd.DataFrame,
-            columns : list[str],
-            mapping_dict : dict) -> pd.DataFrame:
-    
+def mapping(df: pd.DataFrame,
+            columns: list[str],
+            mapping_dict: dict) -> pd.DataFrame:
+    """
+    Apply value mapping to specific columns in a DataFrame.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input DataFrame to be transformed.
+    columns : list[str]
+        List of column names to apply the mapping to.
+    mapping_dict : dict
+        Dictionary defining the mapping (e.g., {'Yes': 1, 'No': 0}).
+
+    Returns
+    -------
+    pd.DataFrame
+        Transformed DataFrame with specified columns mapped.
+    """
     for col in columns:
         df[col] = df[col].map(mapping_dict)
     
     return df
 
-def dummies(df : pd.DataFrame,
-            columns : list[str],
-            to_drop : list[str],
-            prefix : str = '',
-            prefix_sep : str = '') -> pd.DataFrame:
-    
+
+def dummies(df: pd.DataFrame,
+            columns: list[str],
+            to_drop: list[str],
+            prefix: str = '',
+            prefix_sep: str = '') -> pd.DataFrame:
+    """
+    Create one-hot encoded dummy variables for categorical columns and drop unwanted features.
+
+    Parameters
+    ----------
+    df : pd.DataFrame
+        Input DataFrame to be transformed.
+    columns : list[str]
+        List of categorical columns to convert into dummy variables.
+    to_drop : list[str]
+        List of column names (after encoding) to drop from the DataFrame.
+    prefix : str, optional
+        String to append as a prefix to new dummy variable names (default is '').
+    prefix_sep : str, optional
+        Separator between prefix and dummy variable names (default is '').
+
+    Returns
+    -------
+    pd.DataFrame
+        Transformed DataFrame with dummy variables created and specified columns dropped.
+    """
     df = pd.get_dummies(df,
                         columns=columns,
                         prefix=prefix,
                         prefix_sep=prefix_sep)
 
-    df.drop(to_drop, axis=1, inplace=True)
+    df.drop(to_drop, axis=1, inplace=True, errors='ignore')
     
     return df
